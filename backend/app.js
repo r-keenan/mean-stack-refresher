fb9160f8c6dcb4bcdfb6257034a346ee903a0cf7
rkeenan <ross.keenan@protonmail.comm>
2023-04-13 15:33:26 -0500

    Hard coded an array of posts to be returned when hitting the api/posts endpoint.

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First middleware");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send("Hello from Express!");
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "kjsdflkdsjkldfjd",
+      title: "first server-side post",
+      content: "this is coming from the server",
+    },
+    {
+      id: "jlkdsfjkl",
+      title: "second server-side post",
+      content: "this is coming from the server too",
+    },
+  ];
+  res.status(200).json({
+    message: "Posts fetched successfully",
+    posts: posts,
+  });
 });
 
 module.exports = app;
