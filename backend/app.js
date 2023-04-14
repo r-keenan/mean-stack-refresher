4bb34619e1881a09674019dad279bf1c303efc4c
rkeenan <ross.keenan@protonmail.comm>
2023-04-14 17:26:03 -0500

    Added in posting new post and retrieving all posts from the db.

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const Post = require("./models/post");
 
 const app = express();
+
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Houston, we have a problem!");
+  });
 
 //parses json data
 app.use(bodyParser.json());
@@ -17,29 +30,22 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save();
   res.status(201).json({
     message: "Post added successfully.",
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "kjsdflkdsjkldfjd",
-      title: "first server-side post",
-      content: "this is coming from the server",
-    },
-    {
-      id: "jlkdsfjkl",
-      title: "second server-side post",
-      content: "this is coming from the server too",
-    },
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully",
-    posts: posts,
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: "Posts fetched successfully",
+      posts: documents,
+    });
   });
 });
 
