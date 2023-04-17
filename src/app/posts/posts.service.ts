de3f453700fef529e46e981a2cfb16a994e931e4
rkeenan <ross.keenan@protonmail.comm>
2023-04-17 18:50:15 -0500

    Now about to view images in the UI that have been stored on the server.

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -25,6 +25,7 @@ export class PostsService {
               title: post.title,
               content: post.content,
               id: post._id,
+              imagePath: post.imagePath,
             };
           });
         })
@@ -51,15 +52,16 @@ export class PostsService {
     postData.append('content', content);
     postData.append('image', image, title);
     this.httpClient
-      .post<{ message: string; postId: string }>(
+      .post<{ message: string; post: Post }>(
         'http://localhost:3000/api/posts',
         postData
       )
       .subscribe((responseData) => {
         const post: Post = {
-          id: responseData.postId,
+          id: responseData.post.id,
           title: title,
           content: content,
+          imagePath: responseData.post.imagePath,
         };
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
@@ -68,7 +70,12 @@ export class PostsService {
   }
 
   updatePost(id: string, title: string, content: string) {
-    const post: Post = { id: id, title: title, content: content };
+    const post: Post = {
+      id: id,
+      title: title,
+      content: content,
+      imagePath: null,
+    };
     this.httpClient
       .put(`http://localhost:3000/api/posts/${id}`, post)
       .subscribe((response) => {
