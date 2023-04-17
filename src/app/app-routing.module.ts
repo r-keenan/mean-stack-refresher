5f12cb2f6c933c25d8e4d51023e1cefc0482cd6b
rkeenan <ross.keenan@protonmail.comm>
2023-04-17 08:43:10 -0500

    Added in client-side routing.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PostListComponent } from './posts/posts-list/post-list.component';
+import { PostCreateComponent } from './posts/posts-create/posts-create.component';
+
+const routes: Routes = [
+  { path: '', component: PostListComponent },
+  { path: 'create', component: PostCreateComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
