cba06c4da8f703093745397d5bffc6f88ccb5506
rkeenan <ross.keenan@protonmail.comm>
2023-04-17 12:44:05 -0500

    Added in image upload and preview.

--- src/app/posts/posts-create/posts-create.component.ts
+++ src/app/posts/posts-create/posts-create.component.ts
@@ -16,6 +16,7 @@ export class PostCreateComponent implements OnInit {
   post: Post;
   isLoading = false;
   form: FormGroup;
+  imagePreview: string;
   private mode = 'create';
   private postId: string;
 
@@ -31,6 +32,9 @@ export class PostCreateComponent implements OnInit {
       }),
       content: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(10)],
+      }),
+      image: new FormControl(null, {
+        validators: [Validators.required],
       }),
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -55,6 +59,17 @@ export class PostCreateComponent implements OnInit {
         this.postId = null;
       }
     });
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
   }
 
   onSavePost() {
