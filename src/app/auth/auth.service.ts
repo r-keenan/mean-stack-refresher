d87c53e16d4cfed89e196c1f02c4a88e61e21b39
rkeenan <ross.keenan@protonmail.comm>
2023-04-25 09:51:03 -0500

    Now able to authenticate request from front to back.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,7 +7,13 @@ import { AuthData } from './auth-data.model';
   providedIn: 'root',
 })
 export class AuthService {
+  private token: string;
+
   constructor(private httpClient: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
 
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
@@ -21,9 +27,10 @@ export class AuthService {
   login(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
     this.httpClient
-      .post('http://localhost:3000/api/user/login', authData)
+      .post<{ token: string }>('http://localhost:3000/api/user/login', authData)
       .subscribe((response) => {
-        console.log(response);
+        const token = response.token;
+        this.token = token;
       });
   }
 }
